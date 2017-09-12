176f0d19f53fac0eda4b16c408c1a62b58124a5f
Manoj Pravakar Saha <manojpr4@gmail.com>
2017-09-11 22:37:38 -0600

    Second commit

--- tokenizer/test.c
+++ tokenizer/test.c
@@ -3,25 +3,25 @@
 #include "mytoc.h"
 
 int main() {
-
-  char t[100];
-
-  char *t1 = "Hello my dog's name is Darwin.";
-  char *t2 = "     Hello   World!    ";
-  char *t3 = "";
-  char *t4 = "          ";
-  char *t5 = " HelloWorld";
-
+    
+    char t[100];
+    
+    char *t1 = "Hello my dog's name is Darwin. ";
+    char *t2 = "     Hello   World!    ";
+    char *t3 = "";
+    char *t4 = "          ";
+    char *t5 = " HelloWorld";
+    
     mytoc(t1, ' ');
     mytoc(t2, ' ');
     mytoc(t3, ' ');
     mytoc(t4, ' ');
     mytoc(t5, ' ');
-  
-  do {
-    write(1, "$", 1);
-    scanf("%[^\n]s", t);
-    mytoc(t, ' ');
-  } while (t[0] != 'e');
-  return 0;
+    
+    do {
+        write(1, "$", 1);
+        scanf("%[^\n]s", t);
+        //mytoc(t, ' ');
+    } while (t[0] != 'e');
+    return 0;
 }
